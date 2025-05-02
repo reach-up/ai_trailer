9cb573bcf9bd7f81a4a442affec05e988f2fa67f
adamou <adamou752@gmail.com>
2025-05-02 02:32:08 +0100

    Fix: add XttsAudioConfig to safe globals for torch 2.6

--- api.py
+++ api.py
@@ -3,13 +3,19 @@ import subprocess
 from TTS.api import TTS
 from TTS.tts.configs.xtts_config import XttsConfig
 from TTS.tts.models.xtts import XttsAudioConfig
+from TTS.config.shared_configs import BaseDatasetConfig
 import torch.serialization
 
 
-torch.serialization.add_safe_globals([XttsConfig, XttsAudioConfig])
+torch.serialization.add_safe_globals([
+    XttsConfig,
+    XttsAudioConfig,
+    BaseDatasetConfig
+])
 
 app = FastAPI()
 
+# Load the TTS model
 tts = TTS(model_name="tts_models/multilingual/multi-dataset/xtts_v2")
 
 @app.post("/generate_trailer")
